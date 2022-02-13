4ba435b7a07779a56fd3ab49737820ed8dae543c
Alexander Koskovich <akoskovich@pm.me>
2022-02-13 16:47:05 +0800

    umi: Make vendor framework compatibiblity matrix common.
    Change-Id: Idb3438ecd126ba8c4b28e4c86a1acc9a42a1a408

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -91,8 +91,9 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.umi
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/vintf/device_framework_matrix.xml \
-    $(DEVICE_PATH)/vintf/xiaomi_framework_matrix.xml
+    $(DEVICE_PATH)/vintf/xiaomi_framework_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/vintf/manifest.xml \
